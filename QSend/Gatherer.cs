aba7e1f1f142df23c3d0b1f816d10617c7dac825
apex95 <dan.sporici@cti.pub.ro>
2015-10-24 17:41:00 +0300

    some more interface; forgot to close a stream...

--- QSend/Gatherer.cs
+++ QSend/Gatherer.cs
@@ -11,19 +11,26 @@ namespace QSend
 
         private FileStream inputStream;
 
+        private int _streamsRead = 0;
+
         public Gatherer(TransferHeader header)
         {
             this.header = header;
             inputStream = new FileStream(header.filePath, FileMode.Open);
 
             chunkSize = 1 + header.fileSize / header.nOfStreams;
-            
+            _streamsRead = 0;   
         }
 
         public byte[] getNextChunk()
         {
             byte[] buffer = new byte[chunkSize];
             inputStream.Read(buffer, 0, (int)chunkSize);
+            _streamsRead++;
+
+
+            if (_streamsRead == header.nOfStreams)
+                inputStream.Close();
 
             return buffer;
         }
--- QSend/MainWnd.cs
+++ QSend/MainWnd.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Drawing;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 
@@ -81,8 +83,6 @@ namespace QSend
 
         private void MainWnd_Load(object sender, EventArgs e)
         {
-            
-
             // handshakeClient (receiver)
             handshakeClient = new DataClient(0, 2700, responseStream, handleTransferHeader, true);
 
@@ -106,6 +106,36 @@ namespace QSend
         }
 
 
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            e.Graphics.DrawRectangle(Pens.CadetBlue, 0, 0, this.Width-1, this.Height-1);
+        }
+
+        private void closeProgram_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int LPAR);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        const int WM_NCLBUTTONDOWN = 0xA1;
+        const int HT_CAPTION = 0x2;
+
+        private void moveWindow(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
 
         
     }
